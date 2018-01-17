d56442d5edc6deaa95421d96d48b333db9815e58
tomaszd <tomaszdudziec@gmail.com>
2018-01-17 15:05:23 +0100

    added nice paracetamol activity

---
+++ app/src/main/java/com/example/tomaszd/drfever/PillDosageActivity.java
@@ -0,0 +1,13 @@
+package com.example.tomaszd.drfever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PillDosageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill_dosage);
+    }
+}
