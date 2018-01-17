4dcdf8b47bf170b8a6e8f81cff5f6d52f8c61111
tomaszd <tomaszdudziec@gmail.com>
2018-01-17 15:14:55 +0100

    added views for pills/ syrup

--- app/src/main/java/com/example/tomaszd/drfever/DosageDetails.java
+++ app/src/main/java/com/example/tomaszd/drfever/DosageDetails.java
@@ -3,8 +3,11 @@ package com.example.tomaszd.drfever;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DosageDetails extends AppCompatActivity {
 
@@ -20,12 +23,32 @@ public class DosageDetails extends AppCompatActivity {
         String paracetamolAmountValue = myIntent.getStringExtra("paracetamolAmount");
         Double amountOfParacetamolMinValue = myIntent.getDoubleExtra("amountOfParacetamolMin",0.0);
         Double amountOfParacetamolMaxValue = myIntent.getDoubleExtra("amountOfParacetamolMax",0.0);
-
         dosageEditText.setText("Get : " + paracetamolAmountValue);
         editMaxParacetamol.setText(String.valueOf(amountOfParacetamolMaxValue));
         editMinParacetamol.setText(String.valueOf(amountOfParacetamolMinValue));
+        //Pills
+        Button buttonPill = (Button) findViewById(R.id.buttonPill);
+        buttonPill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DosageDetails.this, "Pills",
+                        Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(DosageDetails.this, PillDosageActivity.class);
+                DosageDetails.this.startActivity(myIntent);
+            }
+        });
 
-
+        //syrop
+        Button buttonSyrop = (Button) findViewById(R.id.buttonSirop);
+        buttonSyrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DosageDetails.this, "Sirop",
+                        Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(DosageDetails.this, SirupDosageActivity.class);
+                DosageDetails.this.startActivity(myIntent);
+            }
+        });
 
 
     }
