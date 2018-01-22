b6ef669887907862b0d430df577f4913c0168ded
tomaszd <tomaszdudziec@gmail.com>
2018-01-22 23:10:32 +0100

    better layout for ibuprofen

--- app/src/main/java/com/example/tomaszd/drfever/PillDosageIbuprofenActivity.java
+++ app/src/main/java/com/example/tomaszd/drfever/PillDosageIbuprofenActivity.java
@@ -13,8 +13,6 @@ public class PillDosageIbuprofenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pill_dosage_ibuprofen);
-
-
         //W przypadku ibuprofenu na rynku dostępne są tabletki 200 mg i 400 mg (forte, max, itp.)
 
         final EditText editMinIbuprofenPill = (EditText) findViewById(R.id.editMinIbuprofenPill);
