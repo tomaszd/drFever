b2e8637d76d1787eb42d86c5026b6ba80d2e5930
tomaszd <tomaszdudziec@gmail.com>
2018-01-26 22:41:20 +0100

    nice anim for ibuprofen pills

--- app/src/main/java/com/example/tomaszd/drfever/DosageDetailsIbuprofen.java
+++ app/src/main/java/com/example/tomaszd/drfever/DosageDetailsIbuprofen.java
@@ -24,7 +24,7 @@ public class DosageDetailsIbuprofen extends AppCompatActivity {
         final Double amountOfIbuprofenMin = myIntent.getDoubleExtra("amountOfIbuprofenMin", 0.0);
         final Double amountOfIbuprofenMax = myIntent.getDoubleExtra("amountOfIbuprofenMax", 0.0);
         final Button buttonIbuprofenDosageAmount = (Button) findViewById(R.id.buttonIbuprofenDosageAmount);
-        buttonIbuprofenDosageAmount.setText("        " + String.valueOf(amountOfIbuprofenMax) + " ml");
+        buttonIbuprofenDosageAmount.setText("        " + String.valueOf(amountOfIbuprofenMax) + " mg");
         //editMaxIbuprofen.setText(String.valueOf(amountOfIbuprofenMax));
         //editMinIbuprofen.setText(String.valueOf(amountOfIbuprofenMin));
         //txtIbuprofenDetails.setText(ibuprofenDetails);
--- app/src/main/java/com/example/tomaszd/drfever/PillDosageIbuprofenActivity.java
+++ app/src/main/java/com/example/tomaszd/drfever/PillDosageIbuprofenActivity.java
@@ -1,13 +1,13 @@
 package com.example.tomaszd.drfever;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import static java.lang.Math.round;
 
@@ -21,39 +21,42 @@ public class PillDosageIbuprofenActivity extends AppCompatActivity {
 
         final Button buttonIbuprofenDosageAmount = (Button) findViewById(R.id.buttonIbuprofenDosageAmount);
         final Button buttonIbuprofenDosagePillAmount = (Button) findViewById(R.id.buttonIbuprofenDosagePillAmount);
-
+        final Animation shakeButtonAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
         Intent myIntent = getIntent();
         //final Double amountOfIbuprofenMin = myIntent.getDoubleExtra("amountOfIbuprofenMin", 0.0);
         final Double amountOfIbuprofenMax = myIntent.getDoubleExtra("amountOfIbuprofenMax", 0.0);
 
-        buttonIbuprofenDosageAmount.setText(String.valueOf("    " + String.valueOf(amountOfIbuprofenMax) + " mg"));
+        buttonIbuprofenDosageAmount.setText(String.valueOf("        " + String.valueOf(amountOfIbuprofenMax) + " mg"));
         buttonIbuprofenDosagePillAmount.setText("Wcisnij dawke tabletki");
 
         final Button buttonPillIbuprofen200Forte = (Button) findViewById(R.id.buttonPillIbuprofen200Forte);
         final Button buttonPillIbuprofen400Max = (Button) findViewById(R.id.buttonPillIbuprofen400Max);
-
-
         buttonPillIbuprofen200Forte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(PillDosageIbuprofenActivity.this, "Obliczono dawke",
-                        Toast.LENGTH_LONG).show();
                 buttonIbuprofenDosagePillAmount.setText(getDosageAmount(amountOfIbuprofenMax, 200));
+
+                buttonIbuprofenDosagePillAmount.setAnimation(shakeButtonAnimation);
+                buttonPillIbuprofen200Forte.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
+
             }
         });
 
         buttonPillIbuprofen400Max.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(PillDosageIbuprofenActivity.this, "Obliczono dawke",
-                        Toast.LENGTH_LONG).show();
                 buttonIbuprofenDosagePillAmount.setText(getDosageAmount(amountOfIbuprofenMax, 400));
+
+                buttonPillIbuprofen400Max.setAnimation(shakeButtonAnimation);
+                buttonIbuprofenDosagePillAmount.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
             }
         });
     }
 
     @NonNull
     private String getDosageAmount(Double amountOfParacetamol, int dosageinMl) {
-        return String.valueOf(round(10.0 * amountOfParacetamol / dosageinMl) / 10.0);
+        return String.valueOf(round(100.0 * amountOfParacetamol / dosageinMl) / 100.0);
     }
 }
