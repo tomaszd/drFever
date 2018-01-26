0ba1f9efc65ef12d98e67fdd5c1ca07d80229fbf
tomaszd <tomaszdudziec@gmail.com>
2018-01-26 22:59:01 +0100

    added ibuprofen dosage

--- app/src/main/java/com/example/tomaszd/drfever/SirupDosageIbuprofenActivity.java
+++ app/src/main/java/com/example/tomaszd/drfever/SirupDosageIbuprofenActivity.java
@@ -1,9 +1,13 @@
 package com.example.tomaszd.drfever;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import static java.lang.Math.round;
 
@@ -13,34 +17,46 @@ public class SirupDosageIbuprofenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sirup_dosage_ibuprofen);
+        //W przypadku ibuprofenu na rynku dostępne są tabletki 200 mg i 400 mg (forte, max, itp.)
 
-
-        //Ibuprofen występuje w 2 stężeniach – 100 mg/5ml lub 200 mg/5ml (forte). Zatem dziecku podasz 6 ml zwykłego syropu lub 3 ml preparatu forte.
-
-
-        final EditText editMinIbuprofenSirup = (EditText) findViewById(R.id.editMinIbuprofenSirup);
-        final EditText editMaxIbuprofenSirup = (EditText) findViewById(R.id.editMaxIbuprofenSirup);
-
+        final Button buttonIbuprofenDosageAmount = (Button) findViewById(R.id.buttonIbuprofenDosageAmount);
+        final Button buttonIbuprofenDosageSirupAmount = (Button) findViewById(R.id.buttonIbuprofenDosageSirupAmount);
+        final Animation shakeButtonAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
         Intent myIntent = getIntent();
-        final Double amountOfIbuprofenMin = myIntent.getDoubleExtra("amountOfIbuprofenMin", 0.0);
+        //final Double amountOfIbuprofenMin = myIntent.getDoubleExtra("amountOfIbuprofenMin", 0.0);
         final Double amountOfIbuprofenMax = myIntent.getDoubleExtra("amountOfIbuprofenMax", 0.0);
 
-        editMinIbuprofenSirup.setText(String.valueOf(amountOfIbuprofenMin));
-        editMaxIbuprofenSirup.setText(String.valueOf(amountOfIbuprofenMax));
+        buttonIbuprofenDosageAmount.setText(String.valueOf("        " + String.valueOf(amountOfIbuprofenMax) + " ml"));
+        buttonIbuprofenDosageSirupAmount.setText("Syrop");
 
-        final EditText min100 = (EditText) findViewById(R.id.editMin100);
-        final EditText min200 = (EditText) findViewById(R.id.editMin200);
+        final Button buttonsirup_ibuprof_100_5 = (Button) findViewById(R.id.buttonsirup_ibuprof_100_5);
+        final Button buttonsirup_ibuprof_200_5 = (Button) findViewById(R.id.buttonsirup_ibuprof_200_5);
+        buttonsirup_ibuprof_100_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonIbuprofenDosageSirupAmount.setText(getDosageAmount(amountOfIbuprofenMax, 100));
 
-        final EditText max100 = (EditText) findViewById(R.id.editMax100);
-        final EditText max200 = (EditText) findViewById(R.id.editMax200);
+                buttonIbuprofenDosageSirupAmount.setAnimation(shakeButtonAnimation);
+                buttonsirup_ibuprof_100_5.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
 
+            }
+        });
 
-        min100.setText(String.valueOf(round(100.0 * amountOfIbuprofenMin * 5 / 100.0) / 100.0));
-        min200.setText(String.valueOf(round(100.0 * amountOfIbuprofenMin * 5 / 200.0) / 100.0));
+        buttonsirup_ibuprof_200_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonIbuprofenDosageSirupAmount.setText(getDosageAmount(amountOfIbuprofenMax, 200));
 
-        max100.setText(String.valueOf(round(100.0 * amountOfIbuprofenMax * 5 / 100.0) / 100.0));
-        max200.setText(String.valueOf(round(100.0 * amountOfIbuprofenMax * 5 / 200.0) / 100.0));
+                buttonsirup_ibuprof_200_5.setAnimation(shakeButtonAnimation);
+                buttonIbuprofenDosageSirupAmount.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
+            }
+        });
+    }
 
-
+    @NonNull
+    private String getDosageAmount(Double amountOfParacetamol, int dosageinMl) {
+        return String.valueOf(round(100.0 * amountOfParacetamol * 5 / dosageinMl) / 100.0);
     }
 }
