0425e3329d2459f752bfb35615de8d8374d224f6
tomaszd <tomaszdudziec@gmail.com>
2018-01-27 22:42:53 +0100

    added activity for paracetamol sirup

--- app/src/main/java/com/example/tomaszd/drfever/PillDosageActivity.java
+++ app/src/main/java/com/example/tomaszd/drfever/PillDosageActivity.java
@@ -1,10 +1,13 @@
 package com.example.tomaszd.drfever;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import static java.lang.Math.round;
 
@@ -14,41 +17,73 @@ public class PillDosageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pill_dosage);
+        //Paracetamol występuje w najczęstszej 500 mg tabletce, oraz 250 mg (saszetki), 300 mg, i 1 g.
 
+        final Button buttonParacetamolDosageAmount = (Button) findViewById(R.id.buttonParacetamolDosageAmount);
+        final Button buttonParacetamolDosagePillAmount = (Button) findViewById(R.id.buttonParacetamolDosagepillAmount);
+        final Animation shakeButtonAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
         Intent myIntent = getIntent();
-        final Double amountOfParacetamolMin = myIntent.getDoubleExtra("amountOfParacetamolMin", 0.0);
+        //final Double amountOfParacetamolMin = myIntent.getDoubleExtra("amountOfParacetamolMin", 0.0);
         final Double amountOfParacetamolMax = myIntent.getDoubleExtra("amountOfParacetamolMax", 0.0);
 
-        //500 mg tabletce, oraz 250 mg (saszetki), 300 mg, i 1 g
+        buttonParacetamolDosageAmount.setText(String.valueOf("        " + String.valueOf(amountOfParacetamolMax) + " mg"));
+        buttonParacetamolDosagePillAmount.setText("Tabletka");
 
-        //1000
-        final EditText min1000 = (EditText) findViewById(R.id.editMin1000);
-        final EditText max1000 = (EditText) findViewById(R.id.editMax1000);
-        min1000.setText(getDosageAmount(amountOfParacetamolMin, 1000));
-        max1000.setText(getDosageAmount(amountOfParacetamolMax, 1000));
+        final Button button_pill_parac_1000 = (Button) findViewById(R.id.button_pill_parac_1000);
+        final Button button_pill_parac_500 = (Button) findViewById(R.id.button_pill_parac_500);
+        final Button button_pill_parac_300 = (Button) findViewById(R.id.button_pill_parac_300);
+        final Button button_pill_parac_250 = (Button) findViewById(R.id.button_pill_parac_250);
 
-        //500
-        final EditText min500 = (EditText) findViewById(R.id.editMin500);
-        final EditText max500 = (EditText) findViewById(R.id.editMax500);
-        min500.setText(getDosageAmount(amountOfParacetamolMin, 500));
-        max500.setText(getDosageAmount(amountOfParacetamolMax, 500));
 
-        //300
-        final EditText min300 = (EditText) findViewById(R.id.editMin300);
-        final EditText max300 = (EditText) findViewById(R.id.editMax300);
-        min300.setText(getDosageAmount(amountOfParacetamolMin, 300));
-        max300.setText(getDosageAmount(amountOfParacetamolMax, 300));
+        button_pill_parac_1000.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonParacetamolDosagePillAmount.setText(getDosageAmount(amountOfParacetamolMax, 1000));
 
-        //250
-        final EditText min250 = (EditText) findViewById(R.id.editMin250);
-        final EditText max250 = (EditText) findViewById(R.id.editMax250);
-        min250.setText(getDosageAmount(amountOfParacetamolMin, 250));
-        max250.setText(getDosageAmount(amountOfParacetamolMax, 250));
+                buttonParacetamolDosagePillAmount.setAnimation(shakeButtonAnimation);
+                button_pill_parac_1000.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
 
+            }
+        });
+
+        button_pill_parac_500.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonParacetamolDosagePillAmount.setText(getDosageAmount(amountOfParacetamolMax, 500));
+
+                buttonParacetamolDosagePillAmount.setAnimation(shakeButtonAnimation);
+                button_pill_parac_500.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
+
+            }
+        });
+        button_pill_parac_300.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonParacetamolDosagePillAmount.setText(getDosageAmount(amountOfParacetamolMax, 300));
+
+                buttonParacetamolDosagePillAmount.setAnimation(shakeButtonAnimation);
+                button_pill_parac_300.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
+
+            }
+        });
+
+        button_pill_parac_250.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonParacetamolDosagePillAmount.setText(getDosageAmount(amountOfParacetamolMax, 250));
+
+                buttonParacetamolDosagePillAmount.setAnimation(shakeButtonAnimation);
+                button_pill_parac_250.setAnimation(shakeButtonAnimation);
+                view.startAnimation(shakeButtonAnimation);
+
+            }
+        });
     }
-
     @NonNull
     private String getDosageAmount(Double amountOfParacetamol, int dosageinMl) {
-        return String.valueOf(round(10.0 * amountOfParacetamol / dosageinMl) / 10.0);
+        return "        " + String.valueOf(round(100.0 * amountOfParacetamol / dosageinMl) / 100.0) + " tabletki";
     }
 }
